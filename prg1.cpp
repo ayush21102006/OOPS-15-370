098ae362ef7a027bfffbfba8d0a26870cd8a2855
Ayush Kumar Jha <ayushkj21@gmail.com>
2026-08-17 11:07:51 +0530

    prg 1 done

--- prg1.cpp
+++ prg1.cpp
@@ -1,45 +1,28 @@
 #include <iostream>
 using namespace std;
-class Student{
-    private:
-    int RollNo;
-    string Name;
-    float marks;
-    public:
-    void input(){
-    cout << "Enter Roll Number: ";
-        cin >> RollNo;
-        cin.ignore();
-        cout << "Enter Name: ";
-        getline(cin, Name);
 
-        cout << "Enter Marks: ";
+class Student {
+    int roll_no;
+    string name;
+    float marks;
+
+public:
+    void input() {
+        cin >> roll_no;
+        cin >> name;
         cin >> marks;
     }
 
-    void display(){
-        cout<<"Student Details:"<<endl;
-        cout<<"Name:"<<Name<<endl;
-        cout<<"Roll Number:"<<RollNo<<endl;
-        cout<<"Marks:"<<marks;
+    void display() {
+        cout << roll_no << endl;
+        cout << name << endl;
+        cout << marks << endl;
     }
-    void updateMarks(float newMarks) {
-        marks = newMarks;
-    }
-
 };
 
 int main() {
-    Student s1;
-
-    s1.input();
-    s1.display();
-     float newMarks;
-    cout << "\nEnter new marks to update: ";
-    cin >> newMarks;
-
-    s1.updateMarks(newMarks);
-
-    cout << "\nUpdated Record:" << endl;
-    s1.display();
+    Student S;
+    S.input();
+    S.display();
+    return 0;
 }
