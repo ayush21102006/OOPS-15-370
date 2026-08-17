3612f72429aa18a14fcbc0f23e3de63778049b42
Ayush Kumar Jha <ayushkj21@gmail.com>
2026-08-17 11:31:34 +0530

    program 4 done

---
+++ prg4.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> numbers = {10, 20, 30, 40, 50};
+
+    cout << "Elements of the vector are: ";
+    for (auto x : numbers) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
